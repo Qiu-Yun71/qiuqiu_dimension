738f28441986a5c6df7e58ffcca0d1575224bcb4
Qiu-Yun71 <3321839624@qq.com>
2026-04-29 17:58:47 +0800

    api请求

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//主题切换(加上本地存储实现刷新后主题也不改变)
+//第一部分：主题切换(加上本地存储实现刷新后主题也不改变)
 const themechange = document.getElementById('dark');
 const nowtheme = localStorage.getItem('theme');
 
@@ -13,3 +13,58 @@ themechange.addEventListener('click', () => {
     localStorage.setItem('theme', 'current')
 
 })
+
+
+//第二部分：api请求和卡片渲染
+const cardContainer = document.querySelector('.cards')
+
+//加一个显示加载中的元素
+const loading = doucment.createElement('div')
+loading.className = 'loading'  //设置类名便于添加css样式
+loading.textContent = '<strong>加载中......</strong>'
+
+async function fetchAnime() {
+    cardContainer.innerHTML = '';//清空原有内容
+    cardContainer.appendChild(loading)//显示加载中
+    try {
+        const response = await fetch('https://api.bgm.tv/calendar')
+        if (!response.ok) {
+            throw new Error('网络错误')
+        }
+        const data = await response.json()
+
+        //api返回的结果用星期分类了，这里需要聚合一下
+        let allItems = [];
+        data.forEach(day => {
+            if (day.items && Array.isArray(day.items)) {
+                allItems = [...allItems, ...day.items]
+            }
+
+        });
+
+        //排除掉没有封面的动漫信息，取前二十条
+        const animeList = allItems.filter(item => item => item.images && item.images.large).slice(0, 20)
+        if (animeList.length === 0) {
+            cardContainer.innerHTML = '<p>暂无内容</p>'
+            return
+        }
+
+        //进行渲染
+        renderCards(animeList);
+
+    } catch (error) {
+        console.error('获取失败', error)
+        cardsContainer.innerHTML = '<p>数据加载失败，请检查网络后刷新重试。</p>'
+    }
+
+
+}
+
+//渲染函数定义
+function renderCards(animeList) {
+
+}
+
+fetchAnime()
+
+
